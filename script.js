7ea53881513a2ba4bf89e345f49604f90a3a1038
suhasml <suhasml37@gmail.com>
2022-11-02 20:31:54 +0530

    updates

--- script.js
+++ script.js
@@ -169,6 +169,17 @@
 const navbar = document.getElementsByClassName('navButton')
 for (let i = 0; i < navbar.length; i++) {
   navbar[i].addEventListener('click', () => {
-    document.getElementById(`question${i}`).scrollIntoView({ behavior: 'smooth' })
+    const element = document.getElementById(`question${i}`);
+    const offset = 165;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+    
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+    //document.getElementById(`question${i}`).scrollIntoView({ behavior: 'smooth' })
   })
 }
